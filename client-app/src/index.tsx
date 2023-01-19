5142c72ba007898ee9406b02c7e67829d8b9b869
sunpa <spin2k3@hotmail.com>
2023-01-18 16:35:48 -0800

    End of section 9

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
+import 'react-calendar/dist/Calendar.css';
 import './app/layout/styles.css';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
@@ -14,6 +15,14 @@ root.render(
     <RouterProvider router={router} />
   </StoreContext.Provider>    
 );
+
+// ReactDOM.render(
+//   <StoreContext.Provider value={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </StoreContext.Provider>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
